6aedcf50553b13cd19160766bb44347e382af8ce
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-08-27 13:25:49 +0200

    Fuse to Scene
    added fuse to black animation on scene change

---
+++ The Cavern/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NextLevel : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject fuse;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            fuse.SetActive(true);           
+        }
+    }    
+}
