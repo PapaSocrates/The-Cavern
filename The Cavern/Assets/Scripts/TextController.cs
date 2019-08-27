923f35102aace3c9d3543dba00ace3bb9de01ec3
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-08-27 22:40:54 +0200

    Narrator Text
    Added sample text with animations for narrator

---
+++ The Cavern/Assets/Scripts/TextController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetTrigger("Out");
+        }
+    }
+
+    public void disapear()
+    {
+        gameObject.SetActive(false);
+    }
+}
