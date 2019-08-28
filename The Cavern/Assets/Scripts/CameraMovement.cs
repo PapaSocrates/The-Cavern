4bd7d3e2da73ade0cfa09e1425fef6d3e2ae178d
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-08-28 18:57:02 +0200

    Update
    Correction to player sprint
    Added camera movement script

---
+++ The Cavern/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float smoothTime = .15f;
+
+    [SerializeField]
+    public float MaxX, MinX, MaxY, MinY;
+    private Transform Objetivo;
+
+    float offsety;
+    float offsetx;
+    private Vector3 pos = new Vector3();
+    Vector3 velocidad = Vector3.zero;
+
+    void Start()
+    {
+        Objetivo = GameObject.FindGameObjectWithTag("Player").transform;
+        offsety = 0;
+        offsetx = 0;
+    }
+
+    void FixedUpdate()
+    {
+        try
+        {
+            pos = Objetivo.position + new Vector3 (offsetx,offsety,0);
+        }
+        catch (Exception e) { }
+
+
+        if (pos != null)
+        {
+            pos.y = Mathf.Clamp(pos.y, MinY, MaxY);
+            pos.x = Mathf.Clamp(pos.x, MinX, MaxX);
+            pos.z = transform.position.z;
+            transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocidad, smoothTime);
+        }
+    }
+}
