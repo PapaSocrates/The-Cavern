9aa134ed7309f5c6cc9673ecbb2de8e4b7802139
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-08-28 23:16:27 +0200

    Dead effects
    Added light dead effects to player

---
+++ The Cavern/Assets/Scripts/DeathLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathLight : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject startPoint;
+    public GameObject player;
+
+    public void lightOut()
+    {
+        player.transform.position = startPoint.transform.position;
+        GetComponent<Animator>().SetTrigger("Reduce");
+    }
+
+    public void disableLight()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- The Cavern/Assets/Scripts/PlayerCollisions.cs
+++ The Cavern/Assets/Scripts/PlayerCollisions.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerCollisions : MonoBehaviour
 {
     [SerializeField]
-    public GameObject startPoint;
+    public GameObject light;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Enemy"))
         {
-            gameObject.transform.position = startPoint.transform.position;
+            light.SetActive(true);           
         }
-    }
+    }  
 }
