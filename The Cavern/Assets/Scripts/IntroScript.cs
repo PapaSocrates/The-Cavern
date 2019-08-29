83521a54fd5915a63ea18d27b03b827357da691c
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-08-29 19:28:14 +0200

    Intro

---
+++ The Cavern/Assets/Scripts/IntroScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class IntroScript : MonoBehaviour
+{
+    VideoPlayer player;
+
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player.isPlaying)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            player.Stop();
+            SceneManager.LoadScene(1);
+        }
+    }
+}
